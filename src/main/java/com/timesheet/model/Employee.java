04d624d6218f2e95b06cac563d1082c167cc9013
veritaserg <infophotomasterscomua@gmail.com>
2019-02-05 23:52:30 +0200

    create model

---
+++ src/main/java/com/timesheet/model/Employee.java
@@ -0,0 +1,42 @@
+package com.timesheet.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "department_id")
+    private Long departmentId;
+
+    @Column(name = "position_id")
+    private Long positionId;
+
+    @Column(name = "hourly_rate")
+    private BigDecimal hourlyRate;
+
+    @Column(name = "user_id")
+    private Long userId;
+}
